bfe600f62392077f891eea95d8d1b5f123deaf22
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-08 09:12:35 -0300

    Criado nosso primeio object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/settings/colors.css';
 import './styles/generic/reset.css';
+import './styles/generic/normalize.css';
 import './styles/elements/base.css';
 import App from './App';
 
