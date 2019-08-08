55ca42c0a9241c333452b79eec0c6ece6c5813d5
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-08 17:42:39 -0300

    Finalizado o header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/settings/colors.css';
 import './styles/generic/reset.css';
-import './styles/generic/normalize.css';
+import './styles/generic/fonts.css';
 import './styles/elements/base.css';
 import App from './App';
 
