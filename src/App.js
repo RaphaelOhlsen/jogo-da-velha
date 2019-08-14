55c5493f460cf0dc154c02d1046d87f81059474c
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-14 10:52:06 -0300

    Criado backgroud de AboutPage e sobrepondo a MainPage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Checkbox from './objects/Checkbox';
+import AboutPage from './objects/AboutPage';
 
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+
 
 
 const App = () => (
@@ -12,6 +14,7 @@ const App = () => (
     <Header />
     <Hashtag />
     <Checkbox id="show" value="show" content="Mostrar eventos" />
+    <AboutPage />
   </main>
 );
 
---
+++ src/objects/AboutPage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const AboutPage = () => (
+  <div className="about-page"></div>
+);
+
+export default AboutPage; 
