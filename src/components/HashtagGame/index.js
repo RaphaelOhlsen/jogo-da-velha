15e5c6c91f07dd6663d022514d5d9a092cff5006
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-21 20:09:13 -0300

    Desafio indermediario 54 mas com os estados no componte HashTag

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 import PlayerGame from '../../objects/PlayerGame'
 import CardGame from '../../objects/CardGame';
 
-const HashtagGame = () => (
-  <CardGame>
-    <ul className="hashtag-game">
-      <li className="item"><PlayerGame /></li>
-      <li className="item"><PlayerGame /></li>
-      <li className="item"><PlayerGame /></li>
+const HashtagGame = () => {
 
-      <li className="item"><PlayerGame /></li>
-      <li className="item"><PlayerGame /></li>
-      <li className="item"><PlayerGame /></li>
- 
-      <li className="item"><PlayerGame /></li> 
-      <li className="item"><PlayerGame /></li>
-      <li className="item"><PlayerGame /></li>
-    </ul>
-  </CardGame>
-);
+  const [ xIsNext, setXIsNext] = useState(true);
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+
+  const handleClick = (i) => {
+    const squaresCopy = squares.slice();
+    squaresCopy[i] = xIsNext ? 'x' : 'o';
+    setSquares(squaresCopy);
+    setXIsNext(!xIsNext);
+  }
+
+  const renderSquare = (i) => {
+    return (
+      <PlayerGame
+        value = {squares[i]} 
+        onClick = {() => handleClick(i)}
+      />
+    );
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  const winner = calculateWinner(squares);
+  console.log(winner);
+
+  return (
+    <CardGame>
+      <ul className="hashtag-game">
+        <li className="item">{renderSquare(0)}</li>
+        <li className="item">{renderSquare(1)}</li>
+        <li className="item">{renderSquare(2)}</li>
+
+        <li className="item">{renderSquare(3)}</li>
+        <li className="item">{renderSquare(4)}</li>
+        <li className="item">{renderSquare(5)}</li>
+  
+        <li className="item">{renderSquare(6)}</li> 
+        <li className="item">{renderSquare(7)}</li>
+        <li className="item">{renderSquare(8)}</li>
+      </ul>
+    </CardGame>
+  );  
+};
 
 export default HashtagGame;
--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles.css';
 import playerX from '../../img/player-x.png'
 import playerO from '../../img/player-o.png'
-let actualPlayer = "x";
 
-const PlayerGame = ({player = false}) => {
-  const [statePlayer, setStatePlayer] = useState(player);
-
+const PlayerGame = ({ value, onClick}) => {
   const players = {
     x: playerX,
     o: playerO
   };
 
-  const handleClick = () => {
-    if (!statePlayer && actualPlayer === "x" ) {
-      setStatePlayer("x");
-      actualPlayer = "o";
-    } else if (!statePlayer && actualPlayer === "o") {
-      setStatePlayer("o");
-      actualPlayer = "x";
-    }
-  };
-
    return (
-    <button onClick={handleClick} className="player-game">
-      {statePlayer && 
+    <button onClick={onClick} className="player-game">
+      {value && 
         <img 
-          src={players[statePlayer]} 
-          alt={`Jogador ${statePlayer.toUpperCase()}`}
+          src={players[value]} 
+          alt={`Jogador ${value.toUpperCase()}`}
         />
       }
     </button>
